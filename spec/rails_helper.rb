be9622726454667ae36cd2180cf74a87ffbbb77c
faraznaeem <faraznaeem@gmail.com>
2020-04-20 13:07:26 +0200

    Adds coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,7 @@
+require 'coveralls'
+Coveralls.wear!('rails')
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
